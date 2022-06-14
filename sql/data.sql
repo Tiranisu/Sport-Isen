c071c2c9131a7010564f3765a2e71e67c1632a8e
Mael G_N <mael.grelneau@gmail.com>
2022-06-14 10:01:56 +0000

    add data

--- sql/data.sql
+++ sql/data.sql
@@ -5,18 +5,25 @@ DELETE FROM notifications;
 DELETE FROM sports;
 DELETE FROM address_match;
 
+
+-- --- Populate users table ------------
 ALTER SEQUENCE users_id_seq RESTART;
 INSERT INTO users (id, fistname, lastname, age, email, passwd, city, link_image, nb_match, fitness_id, notifications, notifications_list) VALUES
 (),
 ();
 
-
+-- --- Populate fitness table ------------
 ALTER SEQUENCE fitness_id_seq RESTART;
 INSERT INTO fitness (type) VALUES
 ("Begenner"),
 ("Intermediate"),
 ("Advenced");
 
-
+-- --- Populate notifications table ------------
+ALTER SEQUENCE notifications_id_seq RESTART;
+INSERT INTO notifications (text_notification) VALUES
+(""),
+("Intermediate"),
+("Advenced");
 
 
