6dea57c4ae7ffc7f1e08eb359160ab39eee76e2f
Mael G_N <mael.grelneau@gmail.com>
2022-06-22 11:37:48 +0200

    change : design

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-document.getElementById("cmtx_comment").value = localStorage.getItem("comment");
-
-function saveComment() {
-    var comment = document.getElementById("cmtx_comment").value;
-    if (comment == "") {
-        alert("Please enter a comment in first!");
-        return false;
-    }
-
-    localStorage.setItem("comment", comment);
-    alert("Your comment has been saved!");
-
-    location.reload();
-    return false;
-    //return true;
-}
-
-$(document).ready(function() {
-    $(document).on('submit', '#my-form', function() {
-      console.log("wow");
-      return false;
-     });
-});
+document.querySelectorAll(".carousel").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHtml = Array.from(items, () => {
+      return `<span class="carousel__button"></span>`;
+    });
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="carousel__nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".carousel__button");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        // un-select all the items
+        items.forEach((item) =>
+          item.classList.remove("carousel__item--selected")
+        );
+        buttons.forEach((button) =>
+          button.classList.remove("carousel__button--selected")
+        );
+  
+        items[i].classList.add("carousel__item--selected");
+        button.classList.add("carousel__button--selected");
+      });
+    });
+  
+    // Select the first item on page load
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
+  });
+  
